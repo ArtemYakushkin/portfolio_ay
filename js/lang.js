40e2d4adc6af258485cbc69c6f044d78a589e599
ArtemYakushkin <zimberger29101985@gmail.com>
2024-01-13 00:52:49 +0200

    add ukranian language

--- js/lang.js
+++ js/lang.js
@@ -67,40 +67,149 @@ const langArr = {
         "en": "My Resume",
         "ua": "Моє Резюме",
     },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-    // "": {
-    //     "en": "",
-    //     "ua": "",
-    // },
-}
+    "about-title": {
+        "en": "That's",
+        "ua": "Такий",
+    },
+    "me": {
+        "en": "Me",
+        "ua": "Я",
+    },
+    "about-text": {
+        "en": "What do we have here? Yes, it's me - Artem Yakushkin, your best friend and the one who made this website. Well, it's time to discover a bit about the genius thing next to this page. First, about me: I'm a great developer (at least in my dreams) with a massive ego and a light sense of humor. My life consists of late-night coding including sometimes attempts to find that pesky bug in the code, hiding like a devil under the table. After endless hours working on projects, I spend my time to understand what 'life beyond code' means. But forget about it, who needs that anyway? I have a world of binary code and the sizzling horizons of the command line. So, if you are absorbed with those endless lines of code as I am and want to share your adventures in the world of technical skills, welcome to my dull world. Maybe we can exchange some small stories about how computers make our lives interesting. Right?",
+        "ua": "Що ми тут маємо? Так, це я - Артем Якушкін, ваш найкращий друг і той, хто створив цей веб-сайт. Ну що ж, пора дізнатися трохи про геніальну річ поруч із цією сторінкою. Перше, про мене: я чудовий розробник (принаймні в моїх мріях) з величезним его і легким почуттям гумору. Моє життя складається з кодування до піздньої ночі, включаючи інколи спроби знайти цю набридливу помилку в коді, яка ховається як диявол під столом. Після нескінченних годин праці над проектами, я витрачаю свій час на те, щоб зрозуміти, що таке 'життя поза кодом'. Але забудьте про це, кому це взагалі потрібно? Я маю світ бінарного коду та горизонти командної строки. Отже, якщо ви, як і я, захоплені нескінченними рядками коду, хочу поділитися з вами своїми пригодами у світі технічних навичок. Ласкаво просимо до мого нудного світу. Можливо, ми зможемо обмінятися дрібними історіями про те, як комп’ютери роблять наше життя цікавішим. Правильно?",
+    },
+    "my": {
+        "en": "My",
+        "ua": "Моя",
+    },
+    "journey": {
+        "en": "Journey",
+        "ua": "Подорож",
+    },
+    "through": {
+        "en": "Through",
+        "ua": "По",
+    },
+    "life": {
+        "en": "Life",
+        "ua": "Життю",
+    },
+    "educatio": {
+        "en": "Education",
+        "ua": "Освіта",
+    },
+    "university": {
+        "en": "Development of Mineral Deposits - Donetsk National Technical University",
+        "ua": "Розробка родовищ корисних копалин - Донецький Національний Технічний Університет",
+    },
+    "engineer": {
+        "en": "Mining engineering, technical development, environmental protection, production management, economic analysis, health and safety at work",
+        "ua": "Гірнича інженерія, технічний розвиток, захист навколишнього середовища, управління виробництвом, економічний аналіз, охорона праці",
+    },
+    "goit": {
+        "en": "Full stack developer - IT School GoIT",
+        "ua": "Full stack розробник - IT School GoIT",
+    },
+    "frontend": {
+        "en": "Front-end development, back-end development, databases, server and hosting, version control, optimization and performance, development and testing",
+        "ua": "Front-end розробка, back-end розробка, бази даних, сервер і хостинг, контроль версій, оптимізація та продуктивність, розробка та тестування",
+    },
+
+    "experience": {
+        "en": "Experience",
+        "ua": "Досвід",
+    },
+    "present": {
+        "en": "2008 - present",
+        "ua": "2008 - до цього дня",
+    },
+    "mining": {
+        "en": "Mining engineer - Donbass excavation company",
+        "ua": "Гірничий інженер - Донбаська екскаваційна компанія",
+    },
+    "management": {
+        "en": "Mining management, personnel management from 30 to 70 people, operational planning, monitoring compliance with labor protection rules, manufacturing documents management, planning and reporting",
+        "ua": "Гірниче управління, управління персоналом від 30 до 70 робітників, оперативне планування, контроль за дотриманням правил охорони праці, ведення виробничої документації, планування та звітність",
+    },
+    "present-school": {
+        "en": "2022 - present",
+        "ua": "2022 - до цього дня",
+    },
+    "freelance": {
+        "en": "Freelance Software Developer - Self-employed",
+        "ua": "Фрілансер – приватний підприємець",
+    },
+    "freedom": {
+        "en": "Freedom to choose projects, diverse projects, multitasking, adaptation to rapidly evolving technologies, responsibility for meeting project deadlines",
+        "ua": "Свобода вибору проектів, різноманітність проектів, багатозадачність, адаптація до технологій, що швидко розвиваються, відповідальність за дотримання термінів проектів",
+    },
+    "just": {
+        "en": "Just a",
+        "ua": "Просто",
+    },
+    "god": {
+        "en": "God",
+        "ua": "Бог",
+    },
+    "of": {
+        "en": "of",
+        "ua": "в",
+    },
+    "knoweledge": {
+        "en": "Knoweledge",
+        "ua": "Знаннях",
+    },
+    "and": {
+        "en": "and",
+        "ua": "та",
+    },
+    "skill": {
+        "en": "Skills",
+        "ua": "Вміннях",
+    },
+    "m": {
+        "en": "My",
+        "ua": "Моя",
+    },
+    "rollercoaster": {
+        "en": "Rollercoaster",
+        "ua": "Карусель",
+    },
+    "v": {
+        "en": "of",
+        "ua": "з",
+    },
+    "project": {
+        "en": "Projects",
+        "ua": "Проектами",
+    },
+    "call": {
+        "en": "Call",
+        "ua": "Зателефонуй",
+    },
+    "or": {
+        "en": "or",
+        "ua": "чи",
+    },
+    "write": {
+        "en": "Write",
+        "ua": "Напиши",
+    },
+    "me2": {
+        "en": "Me",
+        "ua": "Мені",
+    },
+    "number": {
+        "en": "Phone number",
+        "ua": "Телефон",
+    },
+    "email": {
+        "en": "Email Address",
+        "ua": "Почта",
+    },
+    "telegram": {
+        "en": "Telegram",
+        "ua": "Телеграм",
+    },
+};
